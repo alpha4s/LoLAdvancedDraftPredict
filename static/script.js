d119bfd763c502ff01288be0e115771ba998ed68
Isa Jamadar <m.isa4ce@gmail.com>
2026-07-28 17:04:24 -0400

    comment

--- static/script.js
+++ static/script.js
@@ -3,7 +3,6 @@ const BACKEND_URL = isLocal ? '' : 'https://loladvanceddraftpredict.onrender.com
 
 fetch(`${BACKEND_URL}/api/predict`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: '{}' }).catch(() => {});
 
-// Heartbeat ping every 10 minutes to keep Render awake while tab is open
 setInterval(() => {
     fetch(`${BACKEND_URL}/api/predict`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: '{}' }).catch(() => {});
 }, 10 * 60 * 1000);
